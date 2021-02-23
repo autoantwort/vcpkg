ae1e1ff34959338bc285ecf8096d4586819255be
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-02-23 17:38:59 +0100

    without check

--- ports/boost-context/b2-options.cmake
+++ ports/boost-context/b2-options.cmake
@@ -1,7 +1,5 @@
-
-if(VCPKG_TARGET_IS_WINDOWS)
-list(APPEND B2_OPTIONS
-    abi=ms
-    binary-format=pe
-)
-endif()
+message(STATUS "VCPKG_TARGET_IS_WINDOWS: ${VCPKG_TARGET_IS_WINDOWS}")
+    list(APPEND B2_OPTIONS
+        abi=ms
+        binary-format=pe
+    )
