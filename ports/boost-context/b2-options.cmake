7c278e2bb5c4c87d4ecc6630ab2c9e064d45940e
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-02-23 18:19:37 +0100

    fix syntax

--- ports/boost-context/b2-options.cmake
+++ ports/boost-context/b2-options.cmake
@@ -1,10 +1,10 @@
 message(STATUS "VCPKG_TARGET_IS_WINDOWS: ${VCPKG_TARGET_IS_WINDOWS}")
 if(${VCPKG_TARGET_IS_WINDOWS})
     message(STATUS "test1")
-endif
+endif()
 if(VCPKG_TARGET_IS_WINDOWS)
     message(STATUS "test2")
-endif
+endif()
 if(VCPKG_TARGET_IS_WINDOWS)
     list(APPEND B2_OPTIONS
         abi=ms
