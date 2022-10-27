86280728b995044eece8cf8c663357abe4e7b9c8
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2022-10-27 23:56:42 +0200

    test change

--- ports/oatpp-sqlite/portfile.cmake
+++ ports/oatpp-sqlite/portfile.cmake
@@ -22,3 +22,5 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+
